9447c6dbc69ed38098bc4e083de360af0fde4adb
kraktorist <1@1.ru>
2023-11-27 20:20:06 +0300

    terraform group and nodes

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,45 +19,79 @@ resource "yandex_vpc_subnet" "network" {
 }
 
 resource "yandex_container_registry" "registry" {
-  name = try(local.config.registry.name, "registry")
+  for_each = local.config.registries
+  name = each.key
 
-  labels = try(local.config.registry.labels, {})
+  labels = try(each.value.labels, {})
 
 }
 
 data "yandex_compute_image" "coi" {
-  family = try(local.config.instance_group.image_family, "container-optimized-image")
+  for_each = local.config.instance_groups
+  family = try(each.value.image_family, "container-optimized-image")
 }
 
-resource "yandex_iam_service_account" "ig-account" {
-  name     = local.config.instance_group.service_account
+resource "yandex_iam_service_account" "service-account" {
+  for_each = local.config.service_accounts
+  name     = each.key
 }
+
+locals {
+  permissions = flatten([
+    for name,service_account in local.config.service_accounts: [
+      for entry in service_account.roles : {
+        sa_name       = name
+        role = entry
+      }
+    ]
+  ])
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "ig-roles" {
+  for_each = {
+      for entry in local.permissions: "${entry.sa_name}-${entry.role}" => entry
+    }
+  folder_id = local.config.folder_id
+  member    = "serviceAccount:${yandex_iam_service_account.service-account[each.value.sa_name].id}"
+  role      = each.value.role
+}
+
+locals {
+  metadata = {
+    for ig_name, instance_group in merge(local.config.instance_groups, local.config.instances):
+      ig_name => {
+        docker-compose = coalescelist([for key, entry in instance_group.metadata: file(entry.file) if key == "docker-compose"], [null])[0]
+        ssh-keys = coalescelist([for key, entry in instance_group.metadata: "${entry.username}:${file(entry.file)}" if key == "ssh-keys"], [null])[0]
+      }
+  }
+}
+
 
 resource "yandex_compute_instance_group" "group" {
-  name                = local.config.instance_group.name
-  service_account_id  = yandex_iam_service_account.ig-account.id
+  for_each = local.config.instance_groups
+  name                = each.key
+  service_account_id = [ for v in yandex_iam_service_account.service-account: v.id if each.value.service_account == v.name ][0]
   instance_template {
     platform_id = "standard-v2"
     resources {
-      cores         = local.config.instance_group.resources.cores
-      memory        = local.config.instance_group.resources.memory
-      core_fraction = try(local.config.instance_group.resources.core_fraction, 5)
+      cores         = each.value.resources.cores
+      memory        = each.value.resources.memory
+      core_fraction = try(each.value.resources.core_fraction, 5)
     }
     boot_disk {
       initialize_params {
         type = "network-hdd"
-        size = local.config.instance_group.resources.disk_size
-        image_id = data.yandex_compute_image.coi.id
+        size = each.value.resources.disk_size
+        image_id = data.yandex_compute_image.coi[each.key].id
       }
     }
     network_interface {
-      subnet_ids = [ for v in yandex_vpc_subnet.network: v.id if local.config.instance_group.network.subnet == v.name ]
-      nat = local.config.instance_group.network.nat
+      subnet_ids = [ for v in yandex_vpc_subnet.network: v.id if each.value.network.subnet == v.name ]
+      nat = try(each.value.network.nat, false)
     }
-    metadata = {
-      foo      = "bar"
-      ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-    }
+    
+    metadata = local.metadata[each.key]
+
     network_settings {
       type = "STANDARD"
     }
@@ -65,17 +99,52 @@ resource "yandex_compute_instance_group" "group" {
 
   scale_policy {
     fixed_scale {
-      size = local.config.instance_group.node_count
+      size = each.value.node_count
     }
   }
 
   allocation_policy {
-    zones = local.config.instance_group.allocation_policy
+    zones = each.value.allocation_policy
   }
 
   deploy_policy {
-    max_unavailable = local.config.instance_group.deploy_policy.max_unavailable
-    max_expansion   = local.config.instance_group.deploy_policy.max_expansion
+    max_unavailable = each.value.deploy_policy.max_unavailable
+    max_expansion   = each.value.deploy_policy.max_expansion
   }
+  depends_on = [
+    yandex_iam_service_account.service-account,
+    yandex_resourcemanager_folder_iam_member.ig-roles
+  ]
 }
 
+data "yandex_compute_image" "image" {
+  for_each = local.config.instances
+  family = try(each.value.image_family, "container-optimized-image")
+}
+
+resource "yandex_compute_instance" "instance" {
+  for_each = local.config.instances
+  name        = each.key
+  platform_id = "standard-v2"
+
+    resources {
+      cores         = each.value.resources.cores
+      memory        = each.value.resources.memory
+      core_fraction = try(each.value.resources.core_fraction, 5)
+    }
+
+    boot_disk {
+      initialize_params {
+        type = "network-hdd"
+        size = each.value.resources.disk_size
+        image_id = data.yandex_compute_image.image[each.key].id
+      }
+    }
+
+    network_interface {
+      subnet_id = [ for v in yandex_vpc_subnet.network: v.id if each.value.network.subnet == v.name ][0]
+      nat = try(each.value.network.nat, false)
+    }
+
+  metadata = local.metadata[each.key]
+}
