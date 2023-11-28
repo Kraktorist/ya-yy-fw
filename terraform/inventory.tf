6b9ad5d75884fc8fa3e2e5ce61da44a81446ee1c
kraktorist <1@1.ru>
2023-11-28 03:51:27 +0300

    terraform inventory generating

---
+++ terraform/inventory.tf
@@ -0,0 +1,44 @@
+locals {
+  ansible_groups_for_instances = distinct(flatten(values(local.config.instances)[*].ansible_groups))
+  ansible_inventory_for_instances = {
+    for group in local.ansible_groups_for_instances :
+    group => {
+      hosts = { for key, entry in local.config.instances :
+        key => { ansible_host = yandex_compute_instance.instance[key].network_interface[0].ip_address }
+      if contains(entry.ansible_groups, group) }
+    }
+  }
+
+  ansible_groups_for_group_instances = distinct(flatten(values(local.config.instance_groups)[*].ansible_groups))
+  hosts = merge([
+    for k1, v1 in yandex_compute_instance_group.group :
+    {
+      for v2 in v1.instances :
+      v2.name => {
+        group          = k1
+        ip             = v2.network_interface[0].ip_address
+        ansible_groups = local.config.instance_groups[k1].ansible_groups
+      }
+    }
+  ]...)
+
+  ansible_inventory_for_instance_groups = {
+    for group in local.ansible_groups_for_group_instances :
+    group => {
+      hosts = {
+        for key, entry in local.hosts :
+        key => { ansible_host = entry.ip }
+
+        if contains(entry.ansible_groups, group)
+      }
+    }
+  }
+
+  ansible_inventory = merge(local.ansible_inventory_for_instance_groups, local.ansible_inventory_for_instances)
+
+}
+
+resource "local_file" "ansible_inventory" {
+  filename = "xxx.yaml"
+  content  = yamlencode(local.ansible_inventory)
+}
