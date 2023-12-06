abebd6a2966ad837142d28ba30ee0c785a3ddda3
kraktorist <1@1.ru>
2023-12-06 22:20:21 +0300

    ---

--- install.sh
+++ install.sh
@@ -12,18 +12,18 @@ if ! [[ ${ACTION} =~ ^(apply|destroy)$ ]]; then
   exit 1
 fi
 
-terraform_dir=terraform2
+terraform_dir=terraform
 
 source ./scripts/init.sh
 set -euo pipefail
 export ENV
 export TF_VAR_env_folder=$(pwd)/envs/${ENV}
-terraform -chdir=terraform2 init -backend-config="bucket=${S3_TF_STATE}" -reconfigure
-terraform -chdir=terraform2 workspace select ${ENV} || terraform -chdir=terraform2 workspace new ${ENV}
-terraform -chdir=terraform2 ${ACTION}
+terraform -chdir=${terraform_dir} init -backend-config="bucket=${S3_TF_STATE}" -reconfigure
+terraform -chdir=${terraform_dir} workspace select ${ENV} || terraform -chdir=${terraform_dir} workspace new ${ENV}
+terraform -chdir=${terraform_dir} ${ACTION}
 
 # if [ "${ACTION}" = "apply" ]; then
-#     eval $(terraform -chdir=terraform2 output -raw ANSIBLE_SSH_COMMON_ARGS || true)
+#     eval $(terraform -chdir=${terraform_dir} output -raw ANSIBLE_SSH_COMMON_ARGS || true)
 #     ansible-playbook -i envs/${ENV}/inventory.yaml ansible/playbook.yml
-#     terraform -chdir=terraform2 output
+#     terraform -chdir=${terraform_dir} output
 # fi
